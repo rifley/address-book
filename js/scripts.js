9984549db8221c14248e9166242f5ab176f49cf9
Christian Martinez and Xi Xia <christian@>
2017-02-13 10:32:54 -0800

    create index.html and link scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var add = function(number1,number2) {
+	return number1 + number2;
+};
+
+var number1 = parseInt(prompt ("Enter your first number"));
+var number2 = parseInt(prompt ("Enter your second number"));
+
+alert(add(number1,number2));
