eca06840f52e19908f41f125c69565fd9efbbc87
Riley Watts and Xi Xia <xi@>
2017-02-15 11:30:53 -0800

    seperate front & back end logic in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,33 @@
+
+// back end business logic
 var add = function(number1,number2) {
 	return number1 + number2;
 };
 
-var number1 = parseInt(prompt ("Enter your first number"));
-var number2 = parseInt(prompt ("Enter your second number"));
-var result = add(number1,number2);
-alert(result);
-
-
-
-var subtract = function(number1,number2) {
-	return number1 - number2;
+var subtract = function(number1, number2) {
+return number1 - number2;
 };
 
-var number1 = parseInt(prompt ("Enter your first number"));
-var number2 = parseInt(prompt ("Enter your second number"));
-var result = subtract(number1,number2);
-alert(result);
-
-
-var multiply = function(number1,number2) {
-	return number1 * number2;
+var multiply = function(number1, number2) {
+return number1 * number2;
 };
 
-var number1 = parseInt(prompt ("Enter your first number"));
-var number2 = parseInt(prompt ("Enter your second number"));
-var result = multiply(number1,number2);
-alert(result);
-
-
-var divide = function(number1,number2) {
-	return number1 / number2;
+var divide = function(number1, number2) {
+return number1 / number2;
 };
 
-var number1 = parseInt(prompt ("Enter your first number"));
-var number2 = parseInt(prompt ("Enter your second number"));
-var result = divide(number1,number2);
-alert(result);
+
+//front end of calculator app
+
+$(document).ready(function() {
+	$("form#add").submit(function (event){
+		event.preventDefault();
+
+		var number1 = parseInt($("#add1").val());
+		var number2 = parseInt($("#add2").val());
+	  var result = add(number1, number2);
+		$("#output").text(result);
+
+	});
+
+});
