6def836c0c3a3d30ed66639e1335ef2b54e0e9e6
Riley Watts and Xi Xia <xi@>
2017-02-15 15:33:22 -0800

    basic address book started

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,15 @@
 //front end of calculator app
 
 $(document).ready(function() {
-	$("form#appointment").submit(function (event){
+	$("form#friends").submit(function (event){
 		event.preventDefault();
-		var firstNameInput = $("#firstName").val();
-		var lastNameInput = $("#lastName").val();
-		var whyHereInput = $("#whyHere").val();
-		var appointmentDateInput = $("#appointmentDate").val();
-		var startTimeInput = $("#startTime").val();
-		var endTimeInput = $("#endTime").val();
-		$(".output").show();
-		$("#outputText").text(firstNameInput+" "+lastNameInput+", "+"your appointment on "+appointmentDateInput+" starting at "+startTimeInput+" and ending at "+endTimeInput+" is confirmed. Thank you!");
+		var nameInput = $("#name").val();
+		console.log(nameInput);
+		var secretNameInput = $("#secretName").val();
+		$("#friendList").append('<p class="clickable">'+nameInput+"</p>");
+
+		$("#friendList").append("<li>"+secretNameInput+"</li>");
+		$("li").addClass ("test");
 
 	});
 
