c177de89e0cec185feab5b4265327d95e9c949ab
Riley Watts and Xi Xia <riley@>
2017-02-15 13:42:55 -0800

    basic appointment booking form with confirmation message return

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,18 @@
 
 // back end business logic
-var add = function(number1,number2) {
-	return number1 + number2;
-};
-
-var subtract = function(number1, number2) {
-	return number1 - number2;
-};
-
-var multiply = function(number1, number2) {
-	return number1 * number2;
-};
-
-var divide = function(number1, number2) {
-	return number1 / number2;
-};
-
-
 //front end of calculator app
 
 $(document).ready(function() {
-	$("form#add").submit(function (event){
+	$("form#appointment").submit(function (event){
 		event.preventDefault();
-		var number1 = parseInt($("#add1").val());
-		var number2 = parseInt($("#add2").val());
-	  var result = add(number1, number2);
-		$("#output").text(result);
-	});
-
-	$("form#subtract").submit(function (event){
-		event.preventDefault();
-		var number1 = parseInt($("#subtract1").val());
-		var number2 = parseInt($("#subtract2").val());
-	  var result = subtract(number1, number2);
-		$("#output").text(result);
-	});
-
-	$("form#multiply").submit(function (event){
-		event.preventDefault();
-		var number1 = parseInt($("#multiply1").val());
-		var number2 = parseInt($("#multiply2").val());
-	  var result = multiply(number1, number2);
-		$("#output").text(result);
-	});
-
-	$("form#divide").submit(function (event){
-		event.preventDefault();
-		var number1 = parseInt($("#divide1").val());
-		var number2 = parseInt($("#divide2").val());
-	  var result = divide(number1, number2);
-		$("#output").text(result);
+		var firstNameInput = $("#firstName").val();
+		var lastNameInput = $("#lastName").val();
+		var whyHereInput = $("#whyHere").val();
+		var appointmentDateInput = $("#appointmentDate").val();
+		var startTimeInput = $("#startTime").val();
+		var endTimeInput = $("#endTime").val();
+		$(".output").show();
+		$("#outputText").text(firstNameInput+" "+lastNameInput+", "+"your appointment on "+appointmentDateInput+" starting at "+startTimeInput+" and ending at "+endTimeInput+" is confirmed. Thank you!");
 
 	});
 
