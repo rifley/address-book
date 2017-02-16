6d6a1e51215c37c0e4225076c73cadd15d2ebf5a
Riley Watts and Xi Xia <xi@>
2017-02-15 16:48:23 -0800

    break our address book

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,21 @@ $(document).ready(function() {
 	$("form#friends").submit(function (event){
 		event.preventDefault();
 		var nameInput = $("#name").val();
-		console.log(nameInput);
 		var secretNameInput = $("#secretName").val();
 		$("#friendList").append('<p class="clickable">'+nameInput+"</p>");
 
-		$("#friendList").append("<li>"+secretNameInput+"</li>");
-		$("li").addClass ("test");
+		$("#friendList").append("<li id="+nameInput+">"+secretNameInput+"</li>");
+		// $("li").addClass ("showHide");
 
 	});
 
-});
+	$(".clickable").click(function() {
+
+    var targetFriend = "#" + $(this).text();
+		console.log(targetFriend);
+
+		$(targetFriend).toggle();
+
+		});
+
+	});
